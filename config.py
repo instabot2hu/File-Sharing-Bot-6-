cb10459c12d5e125fc7eab669b51b261b77ab728
instabot2hu <sanjugit66@gmail.com>
2025-05-20 08:07:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7369917315:AAFcSWz01I15jobhQU1MH7cHVXKpexp78Cw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", "20342933"))
 API_HASH = os.environ.get("API_HASH", "9233e5deebe6abfc9ba297a9678851be")
 
